9c94cf8f6f530c7bad681174f752a000d73dbcea
Henrique Alves <henriqueadlima@hotmail.com>
2025-07-30 22:56:05 -0300

    Add init.sql to initialize database

---
+++ init/init.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS payments;
+
+CREATE TABLE payments(
+  id SERIAL PRIMARY KEY,
+  correlation_id UUID UNIQUE NOT NULL,
+  amount INTEGER NOT NULL,
+  processed_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
